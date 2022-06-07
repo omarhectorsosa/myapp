0e0ba43c7e8297249840086a430401265c8c936e
Omar Sosa <ososa@psa.com.ar>
2022-06-07 20:11:48 -0300

    Agrega conexion

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
 
-    echo "Hola heroku y github"
+    echo "Hola heroku y github (base de datos)";
+
+    if (!($conexion=mysqli_connect("localhost","user","password","bd"))) { 
+      echo "Error al conectar";
+    } 
+
+    $res = mysqli_query($conexion,"SELECT * FROM product") or die(mysqli_error($conexion)); ;
+
+    while($reg = mysqli_fetch_array($res)){
+        echo '<div class="row text-center b-10 boder-secondary border-radius border-5"><div class="col-3 bg-primary border boder-secondary  border-2">'.$reg["id"].'</div>';
+        echo '<div class="col-3 bg-primary border boder-secondary  border-2">'.$reg["name"].'</div>';
+        echo '<div class="col-3 bg-primary border boder-secondary  border-2">'.$reg["price"].'</div>';
+        echo '<div class="col-3 bg-primary border boder-secondary  border-2">'.$reg["description"].'</div></div>';
+    }
 
 ?>
 
